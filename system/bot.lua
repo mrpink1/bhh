af72ca89e65a23c0ac977c0e3d9ca9e581e18623
mrpink1 <mrpink1@users.noreply.github.com>
2016-11-23 17:06:55 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,19 +234,17 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {62834077,204363060,229598206,270388153,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
-    about_text = [[*IN THE NAME OF ALLAH*
-This is an original bot and based on (AdvanSource, version 4 final).
-Copyright all right reserved and you must respect all laws.
+    about_text = [[*BHH BOT*
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTm
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.ir
-]],
+Source: 
+Channel: @BHH_ch
+Messenger: 
+Creator: @rahbar_salibam & @Hacker44
+Site:
+]]
     --Start rate:
     Group_rate = [[]],
     Supergroup_rate = [[]]
